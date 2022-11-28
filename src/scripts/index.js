0ffd7b065f77b92b8a3612aad14de2aa02d76917
jackson grutka <jgrutka01@gmail.com>
2022-11-28 20:37:13 -0300

    buscando repositorios

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,17 +4,47 @@ async function user(userProfile){
     const response = await fetch(`https://api.github.com/users/${userProfile}`)
     return await response.json()
 }
+async function userRepos(userProfile){
+    const response = await fetch(`https://api.github.com/users/${userProfile}/repos`)
+    return await response.json()
+}
 function getUserProfile(userProfile){
     user(userProfile).then(userData => {
-        let userInfo = `<img src="${userData.avatar_url} alt="foto do perfil do usuario" />"
-                        <div class="data">
-                            <h1>${userData.name ?? "não tem nome cadastrado 😢"}</h1>
-                            <p>${userData.bio ?? "nao tem bio cadastrada 😢"}</p>
+        let userInfo = `<div class="info">
+                            <img src="${userData.avatar_url}" alt="foto do perfil do usuario" />"
+                                <div class="data">
+                                    <h1>${userData.name ?? "não tem nome cadastrado 😢"}</h1>
+                                    <p>${userData.bio ?? "nao tem bio cadastrada 😢"}</p>
+                                </div>
                         </div>`;
+                        
         document.querySelector('.profile-data').innerHTML = userInfo
+    })
+}
+function getUserRepos(userProfile){
+    userRepos(userProfile).then( userRepo => {
+        let repositoriesItens = ""
+        userRepo.forEach(repo => {
+            repositoriesItens += `  <li><a href ="${repo.html_url}">${repo.name}</a></li>`
+        });
+        document.querySelector('.profile-data').innerHTML +=   `<div class ="section repositories">
+                                                                    <h2>Repositories</h2>
+                                                                    <ul>
+                                                                        ${repositoriesItens}
+                                                                    </ul>
+                                                                </div>`
     })
 }
 btn.addEventListener('click', () => {
     let userProfile = input.value
     getUserProfile(userProfile)
+    getUserRepos(userProfile)
+})
+input.addEventListener('keyup', (e) => {
+    const key = e.keyCode
+    let userProfile = e.target.value
+    if (key === 13){
+        getUserProfile(userProfile)
+        getUserRepos(userProfile)
+    }
 })
